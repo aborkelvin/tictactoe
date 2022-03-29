7ce93df4e8b2d07c47be37e6087e4188fa031074
Kelvin <aborkelvin@gmail.com>
2022-03-29 11:59:44 +0100

    Added reinstate to each win

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const player2 = document.querySelector('.player2');
 const restart = document.querySelector('.restart');
 let board = ['','','','','','','','',''];
 let thearray = Array.from(gen);
+let result = document.querySelector('.result')
 /* playerone = {
     marker:'X',
     turn:'o'
@@ -32,36 +33,33 @@ function gameplay(item){
 
 //To check for winners
 function champion(){  
+    let cleararea = function(i){
+        result.textContent = `${board[i]} is a champion`
+    }
     if((board[0] == board[1] && board[1] == board[2] && board[2] != "")){
         alert(`${board[1]} is a champion`)
-        return
+        reinstate();
     }else if(board[3] == board[4] && board[4] == board[5] && board[5] !="") {
         alert(`${board[3]} is a champion`)         
-        return
+        reinstate();
     }else if(board[6] == board[7] && board[7] == board[8] && board[8] !="") {
-        alert(`${board[6]} is a champion`)
-         
-        return
+        alert(`${board[6]} is a champion`)         
+        reinstate();
     }else if(board[0] == board[3] && board[3] == board[6] && board[6] !="") {
-        alert(`${board[3]} is a champion`)
-         
-        return
+        alert(`${board[3]} is a champion`)         
+        reinstate();
     }else if(board[1] == board[4] && board[4] == board[7] && board[7] !="") {
-        alert(`${board[4]} is a champion`)
-         
-        return
+        alert(`${board[4]} is a champion`)         
+        reinstate();
     }else if(board[2] == board[5] && board[5] == board[8] && board[8] !="") {
-        alert(`${board[2]} is a champion`)
-         
-        return
+        alert(`${board[2]} is a champion`)         
+        reinstate();
     }else if(board[0] == board[4] && board[4] == board[8] && board[8] !="") {
-        alert(`${board[0]} is a champion`)
-         
-        return
+        alert(`${board[0]} is a champion`)         
+        reinstate();
     }else if(board[2] == board[4] && board[4] == board[6] && board[6] !="") {
-        alert(`${board[6]} is a champion`)
-         
-        return
+        alert(`${board[6]} is a champion`)         
+        reinstate();
     }else{
         for(let i= 0;i<9;i++){
             if(board[i] == ''){
@@ -69,6 +67,7 @@ function champion(){
             }
         }
         alert('This is a tie');
+        reinstate();
     }
 }
 
@@ -81,10 +80,10 @@ clickers = function(sign){
                     let ind = thearray.indexOf(item);
                     board[ind] = item.innerText;
                 }
-                else{
+                /* else{
                     alert('no')
-                }
-                champion();        
+                } */
+                setTimeout(champion,2000)        
         })
     })
 }();
