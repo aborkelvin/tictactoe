d39076ff0cad304e385c7f8bdb7af7c16619a7d8
Kelvin <aborkelvin@gmail.com>
2022-03-29 10:53:34 +0100

    Changed the gameplay
    The gameboard was removed and all the selectors and major functions
        called on the global space
    Changed the function gameplay to check who's turn it is,then mark with
        the appropriate sign and set the turn to the other's sign
    The IIFE,clickers is now on the global space and calls the gameplay
        for each item,also checks if there are winners yet
    The function champion which checks for winners searches if any of the
        winning patterns are available ties when there's none available and
        boxes are filled
    The restart button calls reinstate function which clears all the boxes
        and the board array and sets the turn back for the first player

--- index.js
+++ index.js
@@ -1,57 +1,102 @@
 const container = document.querySelector('.container');
 let gen = document.querySelectorAll('.gen');
+let turn = 'X';
+const player1 = document.querySelector('.player1');
+const player2 = document.querySelector('.player2');
+const restart = document.querySelector('.restart');
+let board = ['','','','','','','','',''];
+let thearray = Array.from(gen);
+/* playerone = {
+    marker:'X',
+    turn:'o'
+}
+
+playertwo = {
+    marker:'O',
+    turn : 'x'
+}
+ */
+function gameplay(item){
+    if(turn == 'X'){
+        player1.classList.add('inturn')
+        player2.classList.remove('inturn')
+        item.innerText = 'X';
+        turn = 'O'
+    }else if(turn == 'O'){
+        player2.classList.add('inturn')
+        player1.classList.remove('inturn')
+        item.innerText = 'O';
+        turn = 'X'
+    }
+}
+
+//To check for winners
+function champion(){  
+    if((board[0] == board[1] && board[1] == board[2] && board[2] != "")){
+        alert(`${board[1]} is a champion`)
+        return
+    }else if(board[3] == board[4] && board[4] == board[5] && board[5] !="") {
+        alert(`${board[3]} is a champion`)         
+        return
+    }else if(board[6] == board[7] && board[7] == board[8] && board[8] !="") {
+        alert(`${board[6]} is a champion`)
+         
+        return
+    }else if(board[0] == board[3] && board[3] == board[6] && board[6] !="") {
+        alert(`${board[3]} is a champion`)
+         
+        return
+    }else if(board[1] == board[4] && board[4] == board[7] && board[7] !="") {
+        alert(`${board[4]} is a champion`)
+         
+        return
+    }else if(board[2] == board[5] && board[5] == board[8] && board[8] !="") {
+        alert(`${board[2]} is a champion`)
+         
+        return
+    }else if(board[0] == board[4] && board[4] == board[8] && board[8] !="") {
+        alert(`${board[0]} is a champion`)
+         
+        return
+    }else if(board[2] == board[4] && board[4] == board[6] && board[6] !="") {
+        alert(`${board[6]} is a champion`)
+         
+        return
+    }else{
+        for(let i= 0;i<9;i++){
+            if(board[i] == ''){
+                return
+            }
+        }
+        alert('This is a tie');
+    }
+}
 
 
-
-
-
-const gameboard = function(){
-    let gen = document.querySelectorAll('.gen');
-    let board = ['x','o','x','x','o','x','g','g','f']
-    let thearray = Array.from(gen);
-    clickers = function(sign){
-        gen.forEach(function(item){
-            item.addEventListener('click',function(){
-                    item.innerText = sign;
+clickers = function(sign){
+    gen.forEach(function(item){
+        item.addEventListener('click',function(){
+                if(item.innerHTML == ""){
+                    gameplay(item);
                     let ind = thearray.indexOf(item);
-                    board[ind] = sign;
-            })
+                    board[ind] = item.innerText;
+                }
+                else{
+                    alert('no')
+                }
+                champion();        
         })
-    }
-    return{clickers}    
-
-}
-
-let gamestart = gameboard();
-
-
-
-let click1 = 0;
-let click2 = 0;
-const gameplay = function(){
-    const playerone = function(){
-        click1++
-        gamestart.clickers("X"); 
-    }
-    
-    const playertwo = function(){
-        click2++;
-        gamestart.clickers('O');
-    }
-    
-    if(click1 == click2){
-        playerone();
-    }else if(click1>click2){
-        playertwo();
-    } else if(click2>click1){
-        alert('wait for your turn')
-        return;
-    }
-}
-
-
-gen.forEach(function(item){
-    item.addEventListener('click',function(){
-        gameplay();            
     })
+}();
+
+function reinstate(){
+//window.location.reload();
+gen.forEach(function(item){
+    item.innerHTML = '';
+})
+board = ['','','','','','','','','']
+turn = 'X'
+}
+restart.addEventListener('click',function(){
+    reinstate();
 })
